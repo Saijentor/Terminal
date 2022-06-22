c817deff72ce1764368675300d802765623f9073
Sergei Gavrilenko <102379036+Saijentor@users.noreply.github.com>
2022-06-22 18:36:23 +0300

    Update script terminal.sh

--- script terminal.sh
+++ script terminal.sh
@@ -5,5 +5,4 @@ mkdir folder_inner_1 folder_inner_2
 cd folder_inner_1
 touch first.txt second.txt third.txt first.json second.json
 mkdir folder_inner_11 folder_inner_22 folder_inner_33
-ls -la
 mv first.txt first.json folder_inner_33
